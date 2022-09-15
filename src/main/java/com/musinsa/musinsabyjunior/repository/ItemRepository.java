931fa7b0b02746d01131ad8943577b62052b5979
AntonyKoo <kina5418@naver.com>
2022-09-16 02:37:52 +0900

    feat: Add query method @ItemRepository

--- src/main/java/com/musinsa/musinsabyjunior/repository/ItemRepository.java
+++ src/main/java/com/musinsa/musinsabyjunior/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package com.musinsa.musinsabyjunior.repository;
 import com.musinsa.musinsabyjunior.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm);
 }
