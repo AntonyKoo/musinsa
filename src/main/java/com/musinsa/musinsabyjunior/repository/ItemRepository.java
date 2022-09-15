0bd00f082ac8067e620d8baaa7148bd188b9f33a
AntonyKoo <kina5418@naver.com>
2022-09-16 03:28:04 +0900

    feat: Add 상품 조건 별 조회

--- src/main/java/com/musinsa/musinsabyjunior/repository/ItemRepository.java
+++ src/main/java/com/musinsa/musinsabyjunior/repository/ItemRepository.java
@@ -6,5 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    
+    // 상품명으로 조회하는 쿼리
     List<Item> findByItemNm(String itemNm);
+    
+    // Or 조건
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // LessThan 조건
+    List<Item> findByPriceLessThan(Long price);
+
+    // OrderBy 조건
+    List<Item> findByPriceLessThanOrderByPriceDesc(Long price);
 }
