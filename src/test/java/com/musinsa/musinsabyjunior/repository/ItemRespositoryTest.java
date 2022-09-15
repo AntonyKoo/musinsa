0592fec6d3a1c4652206f77b06338e4d392caaac
AntonyKoo <kina5418@naver.com>
2022-09-16 03:02:05 +0900

    test: 상품명 조회 테스트 코드

--- src/test/java/com/musinsa/musinsabyjunior/repository/ItemRespositoryTest.java
+++ src/test/java/com/musinsa/musinsabyjunior/repository/ItemRespositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +33,30 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
 
+    }
+
+    public void createItemList() {
+        for (int i=1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트상품이름" + i);
+            item.setPrice(10000L + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSaleStatus(ItemSaleStatus.ON_SALE);
+            item.setStockNumber(100L);
+            item.setCreatedAt(LocalDateTime.now());
+            item.setModifiedAt(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+    
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findbyItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트상품이름1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 
 }
