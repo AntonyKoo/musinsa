511c8c7a3ed7c30930a9149c81be5d85b0dd770d
AntonyKoo <kina5418@naver.com>
2022-09-15 23:59:42 +0900

    feat: first commit

---
+++ src/main/java/com/musinsa/musinsabyjunior/MusinsaByJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.musinsabyjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaByJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaByJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musinsa/musinsabyjunior/MusinsaByJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.musinsabyjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaByJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
